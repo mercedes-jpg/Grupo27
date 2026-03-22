30f89d1a81f3ec0ce7823f460c79a64d590061aa
Clara <claraisabelsroque@gmail.com>
2026-03-22 15:28:31 +0000

    first commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-//teste12345
+//teste123456
 
+using namespace std;
+
+struct Jogador {
+    string nome;
+    int numero;
+    int idade;
+};
 int main() {
     std::cout << "Hello World!" << std::endl;
 }
