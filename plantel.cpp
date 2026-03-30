bbe644b3d46fc7758ee8bc27ed1681ad19f4516a
Clara <claraisabelsroque@gmail.com>
2026-03-30 17:04:28 +0100

    first commit

--- plantel.cpp
+++ plantel.cpp
@@ -7,14 +7,14 @@
 #include <stdlib.h>
 
 void inicializarPlantel(ListaJogadores &p, int capacidade) {
-    p.jogadores = new Jogador[capacidade];
+    p.jogadores = new Jogador[capacidade]; //hummmmm
     p.tamanho = 0;
     p.capacidade = capacidade;
 }
 
 void inserirJogador(ListaJogadores &p, Jogador j) {
     if (p.tamanho < p.capacidade) { //se o nº de jogadores atual for menor que a capacidade do plantel
-        p.jogadores[p.tamanho] = j; //então vamos inserir o jogador no fim da lista
+        p.jogadores[p.tamanho] = j; //então vamos inserir o jogador no fim da lista   hummmmm
         p.tamanho++; // e o nº atual de jogadores será incrementado num valor
     }
 }
@@ -23,13 +23,13 @@ void removerJogador(ListaJogadores &p, int index) {
     for (int i = index; i < p.tamanho -1; i++) { //i começa no que vamos remover
         p.jogadores[i] = p.jogadores[i+1]; //para nao deixar espaços em branco os jogadores que vêm depois dele vao recuar uma casa isto é esse lugar vai ser do jogador que vem logo de seguida
     }
-    p.tamanho--;
+    p.tamanho--; //hummmmm
 }
 
 void gerarPlantel(ListaJogadores &p) { //ListaJogadores é o tipo, p(plantel) é a variável
     int numero = 1;
 
-    int gr = 2 + rand() % 2; //mínimo 2 GR, máximo 3 GR
+   int gr = 2 + rand() % 2; //mínimo 2 GR, máximo 3 GR
     for (int i = 0; i < gr; i++) { // o i é nos GR
         string nome = obterNomeAleatorio();
         p.jogadores[p.tamanho++] = criarJogador(nome, numero++, "GR"); //p.jogadores -> aceder ao array; p.tamanho++ -> usa o tamanho atual e depois incrementa-o
--- plantel.h
+++ plantel.h
@@ -8,7 +8,7 @@
 #include "jogador.h" // como usamos jogador o compilador precisa saber o que é um jogador; quando usamos uma struct temos de incluir o ficheiro onde ela está definida
 
 struct ListaJogadores {
-    Jogador* jogadores; //array dinâmico
+    Jogador* jogadores; //array dinâmico  hummmmmm
     int tamanho; // nº atual de jogadores
     int capacidade; //capacidade do plantel, ou seja, o nº máximo de jogadores
 };
