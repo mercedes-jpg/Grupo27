c3651c39fb56b9e2ee25e20047e856cbfe01523e
Clara <claraisabelsroque@gmail.com>
2026-03-31 13:21:41 +0100

    first commit

--- plantel.h
+++ plantel.h
@@ -14,6 +14,7 @@ struct ListaJogadores {
 };
 
 void inicializarPlantel(ListaJogadores &p, int capacidade);
+
 void inserirJogador(ListaJogadores &p, Jogador j);
 void removerJogador(ListaJogadores &p, int index);
 void gerarPlantel(ListaJogadores &p); // o & serve para obter o endereço de uma variável na memória (passagem por referência), assim mudamos o plantel verdadeiro; sem & as alteraçaões não ficariam guardadas
