b202e84664fef65f8c759e073f9a76b2cd646ce5
Clara <claraisabelsroque@gmail.com>
2026-04-06 20:30:34 +0100

    first commit

--- equipa.cpp
+++ equipa.cpp
@@ -7,85 +7,85 @@
 using namespace std;
 
 // função para escolher os titulares
-void escolherTitulares(ListaJogadores &plantel, ListaJogadores &titulares)
-{
-    titulares.tamanho = 0;
-    // começamos com 0 titulares (lista vazia) - numero de titulares e 0
-
-    int gr = 0, def = 0, med = 0, ava = 0;
-    // inicialização das variaveis das várias posiçoes a 0
-
-    // percorremos TODOS os jogadores do plantel
-    for (int i = 0; i < plantel.tamanho; i++)
-    {
-        Jogador j = plantel.jogadores[i];
-        // vamos buscar o jogador j na posição i do plantel
-
-        // ignorar jogadores lesionados ou castigados
-        if (j.jornadasLesao > 0 || j.jogosCastigo > 0)
-        {
-            continue;
-            // caso if seja verdadeiro passa a proxima linha de codigo
-        }
-
-        // escolher 1 GR
-        if (j.posicao == "GR" && gr < 1) //j.posição pode ser GR, DEF, MED e AVA, caso seja GR e se ainda não tivermos nenhum guarda-redes, pois ele começa a 0 e se for menor que çlogo zero entao ainda não escolhemos nenhum guarda-redes -> se a pos for GR e ainda não tivermos escolhido nenhum GR então...
-        {
-            titulares.jogadores[titulares.tamanho++] = j;
-            // adiciona o jogador j na posição tamanho e dps incrementa , isto é atualiza o tamanho para mais um à lista titulares
-
-            gr++; // atualiza os GR para mais um
-        }
-
-        // escolher 4 DEF
-        else if (j.posicao == "DEF" && def < 4)
-        {
-            titulares.jogadores[titulares.tamanho++] = j;
-            def++;
-        }
-
-        // escolher 4 MED
-        else if (j.posicao == "MED" && med < 4)
-        {
-            titulares.jogadores[titulares.tamanho++] = j;
-            med++;
-        }
-
-        // escolher 2 AVA
-        else if (j.posicao == "AVA" && ava < 2)
-        {
-            titulares.jogadores[titulares.tamanho++] = j;
-            ava++;
-        }
-
-        // se já tivermos todos os titulares, parar
-        if (gr == 1 && def == 4 && med == 4 && ava == 2)
-        {
-            break; //quebra o ciclo
-        }
-    }
-}
-void escolherSuplentes(ListaJogadores &plantel, ListaJogadores &suplentes)
-{
-    suplentes.tamanho = 0;
-    // lista começa vazia
-
-    // percorre o plantel todo
-    for (int i = 0; i < plantel.tamanho; i++)
-    {
-        Jogador j = plantel.jogadores[i];// vamos buscar o jogador j na posição i do plantel
-
-        // ignorar lesionados/castigados hummmmm
-        if (j.jornadasLesao > 0 || j.jogosCastigo > 0)
-        {
-            continue;
-        }
-
-        // adiciona TODOS (simplificado)
-        suplentes.jogadores[suplentes.tamanho++] = j;
-        // adiciona o jogador j a lista dos suplentes na posição tamanho e dps incrementa 1
-    }
-}
+// void escolherTitulares(ListaJogadores &plantel, ListaJogadores &titulares)
+// {
+//     titulares.tamanho = 0;
+//     // começamos com 0 titulares (lista vazia) - numero de titulares e 0
+//
+//     int gr = 0, def = 0, med = 0, ava = 0;
+//     // inicialização das variaveis das várias posiçoes a 0
+//
+//     // percorremos TODOS os jogadores do plantel
+//     for (int i = 0; i < plantel.tamanho; i++)
+//     {
+//         Jogador j = plantel.jogadores[i];
+//         // vamos buscar o jogador j na posição i do plantel
+//
+//         // ignorar jogadores lesionados ou castigados
+//         if (j.jornadasLesao > 0 || j.jogosCastigo > 0)
+//         {
+//             continue;
+//             // caso if seja verdadeiro passa a proxima linha de codigo
+//         }
+//
+//         // escolher 1 GR
+//         if (j.posicao == "GR" && gr < 1) //j.posição pode ser GR, DEF, MED e AVA, caso seja GR e se ainda não tivermos nenhum guarda-redes, pois ele começa a 0 e se for menor que çlogo zero entao ainda não escolhemos nenhum guarda-redes -> se a pos for GR e ainda não tivermos escolhido nenhum GR então...
+//         {
+//             titulares.jogadores[titulares.tamanho++] = j;
+//             // adiciona o jogador j na posição tamanho e dps incrementa , isto é atualiza o tamanho para mais um à lista titulares
+//
+//             gr++; // atualiza os GR para mais um
+//         }
+//
+//         // escolher 4 DEF
+//         else if (j.posicao == "DEF" && def < 4)
+//         {
+//             titulares.jogadores[titulares.tamanho++] = j;
+//             def++;
+//         }
+//
+//         // escolher 4 MED
+//         else if (j.posicao == "MED" && med < 4)
+//         {
+//             titulares.jogadores[titulares.tamanho++] = j;
+//             med++;
+//         }
+//
+//         // escolher 2 AVA
+//         else if (j.posicao == "AVA" && ava < 2)
+//         {
+//             titulares.jogadores[titulares.tamanho++] = j;
+//             ava++;
+//         }
+//
+//         // se já tivermos todos os titulares, parar
+//         if (gr == 1 && def == 4 && med == 4 && ava == 2)
+//         {
+//             break; //quebra o ciclo
+//         }
+//     }
+// }
+// void escolherSuplentes(ListaJogadores &plantel, ListaJogadores &suplentes)
+// {
+//     suplentes.tamanho = 0;
+//     // lista começa vazia
+//
+//     // percorre o plantel
+//     for (int i = 0; i < plantel.tamanho; i++)
+//     {
+//         Jogador j = plantel.jogadores[i];// vamos buscar o jogador j na posição i do plantel
+//
+//         // ignorar lesionados/castigados hummmmm
+//         if (j.jornadasLesao > 0 || j.jogosCastigo > 0)
+//         {
+//             continue;
+//         }
+//
+//         // adiciona TODOS (simplificado)
+//         suplentes.jogadores[suplentes.tamanho++] = j;
+//         // adiciona o jogador j a lista dos suplentes na posição tamanho e dps incrementa 1
+//     }
+// }
 
 void selecionarEquipa(ListaJogadores &plantel, ListaJogadores &titulares, ListaJogadores &suplentes) {
     inicializarLista(titulares, 11);//inicializa a lista de titulares
@@ -94,7 +94,7 @@ void selecionarEquipa(ListaJogadores &plantel, ListaJogadores &titulares, ListaJ
 
     // titulares 1-4-4-2
     int gr = 0, def = 0, med = 0, ava = 0;//inicialização das diferentes posições a zero
-    for (int i = 0; i < plantel.tamanho; i++) { //percorre se o plnatel usando o seu indice e o que esta abaixo acontece sempre que i<plantel.tamanho ou i<=(plantel.tamanho - 1) sendo que i começa a zero e vai incrementando sempre 1
+    for (int i = 0; i < plantel.tamanho; i++) { //percorre se o plantel usando o seu indice e o que esta abaixo acontece sempre que i<plantel.tamanho ou i<=(plantel.tamanho - 1) sendo que i começa a zero e vai incrementando sempre 1
         Jogador j = plantel.jogadores[i];// jogado j é um jogador que se foi buscar ao plantel de jogadores ou a lista de jogadores no plantel na posição i e por sua vez guarda se na variavel j do tipo jogador
 
         if (j.posicao == "GR" && gr < 1) { //se a posição do jogador j for gr e se ainda não tivermos nenhum guarda redes, pois so precisamos de um gr, então:
